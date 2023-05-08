b8dbb0674321ea1d82e85ba6a2f6dc32cd86b7e7
vikkitorry <vikkitorry6716@gmail.com>
2023-05-08 17:42:51 +1000

    feat: add webpack configs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.html';
+import './index.scss';
+import {sum, sum1} from './modules/calc'
+
+
+console.log(sum(2,4))
+console.log(sum1(2,4))
---
+++ src/modules/calc.js
@@ -0,0 +1,2 @@
+export const sum = (a,b) => a + b;
+export const sum1 = async(a,b) => a + b + a;
